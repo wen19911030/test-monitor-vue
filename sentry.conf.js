f318a7ca381de51aa8d254183fe8f0cf8d6cc994
wen19911030 <wen19911030@gmail.com>
2018-07-16 14:33:13 +0800

    feat:项目初始化

---
+++ sentry.conf.js
@@ -0,0 +1,5 @@
+module.exports = {
+    organisation: '5b94071dc50c', // 可以从URL看出，如`sentry.io/myorg/myprj`，则org是`myorg`
+    project: 'vue', // 如`sentry.mycorp.com/sentry/pc/`，则org是`sentry`，project是`pc`
+    apiKey: 'e908b6d9d29343fe9f5ecade6536ebc5370c2a58f22e47749c35c1323f8f739b'
+}
---
+++ src/main.js
@@ -0,0 +1,27 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+import Raven from 'raven-js';
+import RavenVue from 'raven-js/plugins/vue';
+
+Raven
+    .config('https://a0dcf727f93e40c4a7b1c47f414ab8ab@sentry.io/1243650', {
+        release: process.env.RELEASE_VERSION,
+        debug: true
+    })
+    .addPlugin(RavenVue, Vue)
+    .install();
+
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+    el: '#app',
+    router,
+    components: { App },
+    template: '<App/>'
+})
